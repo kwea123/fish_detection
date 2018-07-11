a550b62e5c4240248f0563ee7ad9c146bc606748
ubuntu <kwea123@gmail.com>
2018-07-11 21:08:29 +0900

    add label map, modify create_tfrecords to contain multiple bboxes in one image

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM tensorflow/tensorflow:latest-gpu-py3
 
+WORKDIR "/root"
+
 # install protobuf 3
 RUN curl -OL https://github.com/google/protobuf/releases/download/v3.2.0/protoc-3.2.0-linux-x86_64.zip
 RUN unzip protoc-3.2.0-linux-x86_64.zip -d protoc3
--- create_tfrecords.py
+++ create_tfrecords.py
@@ -1,28 +1,29 @@
 # Borrowed from https://github.com/tensorflow/models/blob/master/research/object_detection/g3doc/using_your_own_dataset.md
-# Run 'python create_tfrecords.py --output_path data/fish_train.record --csv_input data/fish_train.csv'
+# Run 'python create_tfrecords.py --output_path data/fish_train.record --csv_input data/fish_train.csv --image_path=root/data/images/train/'
 
 import tensorflow as tf
 from utils import dataset_util
 import pandas as pd
-from PIL import Image # to read images
+from PIL import Image
 import os
+import numpy as np
 
 flags = tf.app.flags
 flags.DEFINE_string('csv_input', '', 'Path to csv file')
 flags.DEFINE_string('output_path', '', 'Path to output TFRecord')
+flags.DEFINE_string('image_path', '', 'Path to the images')
 FLAGS = flags.FLAGS
 
-TRAIN_DIR = '/root/data/images/train/' # the directory where training images are located
-
-def create_tf_example(example):
+def create_tf_example(image_path, example, image_id):
     """
     Create a tf_example using @example.
     @example is of form : ["ImageID", "XMin", "XMax", "YMin", "YMax"] which are the columns of "fish.csv".
+    @example contains all bounding boxes for the image with @image_id
     """
     
-    filename = example['ImageID']+'.jpg'
+    filename = image_id+'.jpg'
     
-    image_path = os.path.join(TRAIN_DIR, filename)
+    image_path = os.path.join(image_path, filename)
     with tf.gfile.GFile(image_path, 'rb') as fid:
         encoded_image_data = fid.read()
         
@@ -32,14 +33,19 @@ def create_tf_example(example):
     del image
     
     image_format = b'jpg'
-
-    xmins = [example['XMin']]
-    xmaxs = [example['XMax']]
-    ymins = [example['YMin']]
-    ymaxs = [example['YMax']]
+    xmins = []
+    xmaxs = []
+    ymins = []
+    ymaxs = []
     
-    classes_text = [b'fish']
-    classes = [1]
+    for bbox in np.array(example[['XMin', 'XMax', 'YMin', 'YMax']]):
+        xmins += [bbox[0]]
+        xmaxs += [bbox[1]]
+        ymins += [bbox[2]]
+        ymaxs += [bbox[3]]
+    
+    classes_text = [b'fish']*len(example)
+    classes = [1]*len(example)
 
     tf_example = tf.train.Example(features=tf.train.Features(feature={
       'image/height': dataset_util.int64_feature(height),
@@ -60,14 +66,15 @@ def create_tf_example(example):
 
 def main(_):
     writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
-
+    
     df = pd.read_csv(FLAGS.csv_input)
-    n_examples = len(df)
+    image_id_list = list(set(df['ImageID']))
+    n_examples = len(image_id_list)
 
-    for i in range(n_examples):
-        tf_example = create_tf_example(df.loc[i])
+    for i, image_id in enumerate(image_id_list):
+        tf_example = create_tf_example(FLAGS.image_path, df[df['ImageID']==image_id], image_id)
         writer.write(tf_example.SerializeToString())
-        print('\rprocessing %d of all %d'%(i+1, n_examples), end="")
+        print('\rprocessing %d of all %d images'%(i+1, n_examples), end="")
     print('\nDone!')
     writer.close()
 
